5f8521560002e99435181942c1844bcd1e10bf3e
Adrian  Cuba <adriancuba0512@gmail.com>
2022-05-06 17:02:13 -0400

    fix on routes

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -8,7 +8,7 @@ export const AppRoutes = () => {
     return <Routes>
         <Route path="/" element={<Home />} />
         <Route exact path="/reservar" element={<Book />} />
-        <Route exact path="admin/pendientes" element={<PendingRequests />} />
+        <Route exact path="/admin/pendientes" element={<PendingRequests />} />
         <Route exact path="/admin/reserva" element={<Request />} />
     </Routes>
 
