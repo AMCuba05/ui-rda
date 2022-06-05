c4aacedd4070af069f7b3a8631947cab1b0359c5
Alejandro20032001 <dilanantezama@gmail.com>
2022-06-05 16:45:54 -0400

    integracion notificaciones

--- src/components/Notifications/Pending/index.jsx
+++ src/components/Notifications/Pending/index.jsx
@@ -1,7 +1,7 @@
 import focusIcon from '../../../assets/svg/focus.svg'
 import "./styles.css"
 
-export const NotificationsPending = () =>{
+export const NotificationsPending = ({date}) =>{
   const dateNotification = new Date();
 
   return <div className={'notification-pending-container'}>
@@ -13,7 +13,7 @@ export const NotificationsPending = () =>{
         <div className={'notification-pending-content-text'}>
           <span className={'notification-pending-title'}>Su reserva esta pendiente</span>
           <span className={'notification-pending-description'}> <b> Motivo: </b> Esperando respuesta del administrador</span>
-          <span className={'notification-pending-date'}>14:00 10/5/2022 </span>
+          <span className={'notification-pending-date'}>{date}</span>
         </div>
     </div>
   </div>
--- src/components/Notifications/warning/index.jsx
+++ src/components/Notifications/warning/index.jsx
@@ -1,7 +1,7 @@
 import faceRedIcon from '../../../assets/svg/face-red.svg'
 import "./styles.css"
 
-export const NotificationsWarning = () =>{
+export const NotificationsWarning = ({date}) =>{
   const dateNotification = new Date();
 
   return <div className={'notification-warning-container'}>
@@ -13,7 +13,7 @@ export const NotificationsWarning = () =>{
         <div className={'notification-warning-content-text'}>
           <span className={'notification-warning-title'}>Su reserva esta fue rechazada</span>
           <span className={'notification-warning-description'}> <b> Motivo: </b> Aula no disponible </span>
-          <span className={'notification-warning-date'}>14:00 10/5/2022 </span>
+          <span className={'notification-warning-date'}>{date} </span>
         </div>
     </div>
   </div>
