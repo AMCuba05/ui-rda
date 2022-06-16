9ac4e0cc9d95bd6b8dab569d51efaf29ed9ebe78
Alejandro20032001 <dilanantezama@gmail.com>
2022-06-16 18:38:20 -0400

    integracion con docente

--- src/pages/UserConfigurations/index.jsx
+++ src/pages/UserConfigurations/index.jsx
@@ -3,7 +3,9 @@ import editIcon from '../../assets/svg/edit-icon.svg';
 import './styles.css';
 import { WhiteButton } from '../../components/Buttons/WhiteButton';
 import { CommonButton } from '../../components/Buttons/Common';
+import {CommonInput} from "../../components/Inputs/Common";
 import {useEffect, useState} from "react";
+import { actualizarDocente } from "../../api/modificarDocente";
 
 export const UserConfigurations = () => {
   const [inputName, setInputName] = useState(true);
@@ -11,7 +13,30 @@ export const UserConfigurations = () => {
   const [inputEmail, setInputEmail] = useState(true);
   const [inputCellphone, setInputCellphone] = useState(true);
   const [inputPassword, setInputPassword] = useState(true);
-
+  const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem("user")));
+  const [newEmail, setEmail] = useState()
+  const [newPassword, setPass] = useState()
+  const [newCel, setCel] = useState()
+  const handleEmail = (newEmail) => {
+    setEmail(newEmail)
+    console.log(newEmail)
+  }
+  const handlePass = (newPass) => {
+    setPass(newPass)
+    console.log(newPass)
+  }
+  const handleCel = (newCel) => {
+    setCel(newCel)
+    console.log(newCel)
+  }
+  const modificar = async () =>{
+    if(newCel === undefined) {setCel(currentUser.celular)}
+    if(newEmail === undefined) {setEmail(currentUser.email)}
+    const data = await actualizarDocente(currentUser.id,newEmail,  newCel, newPassword);
+    console.log("algo")
+    console.log(data)
+    return data;
+  }
   return (
     <div className={'user-configurations-page'}>
       <div>
@@ -26,15 +51,14 @@ export const UserConfigurations = () => {
             <div className={'user-configuration-input-label'}>
               <span>Nombre Completo</span>
               <div className={'user-configuration-input'}>
-                <input type="text" disabled={inputName}/>
-                <img src={editIcon} onClick={() => setInputName(false)} />
+                <input type="text" disabled={inputName} placeholder = {currentUser.nombre}/>
               </div>
             </div>
             <div className={'user-configuration-input-label'}>
               <span>Cod Sis</span>
               <div className={'user-configuration-input'}>
-                <input type="text" disabled={inputCodSis}/>
-                <img src={editIcon} onClick={() => setInputCodSis(false)}/>
+                {console.log(currentUser)}
+                <input type="text" disabled={inputCodSis} placeholder={currentUser.cod_SIS}/>
               </div>
             </div>
 
@@ -43,14 +67,14 @@ export const UserConfigurations = () => {
             <div className={'user-configuration-input-label'}>
               <span>Correo</span>
               <div className={'user-configuration-input'}>
-                <input type="email" disabled={inputEmail} />
+                <CommonInput type="email" disabled={inputEmail} placeholder = {currentUser.email} inputChange={handleEmail}/>
                 <img src={editIcon} onClick={() => setInputEmail(false)}/>
               </div>
             </div>
             <div className={'user-configuration-input-label'}>
               <span>Celular</span>
               <div className={'user-configuration-input'}>
-                <input type="number" disabled={inputCellphone}/>
+                <CommonInput type="number" disabled={inputCellphone} placeholder = {currentUser.celular} inputChange={handleCel}/>
                 <img src={editIcon} onClick={() => setInputCellphone(false)}/>
               </div>
             </div>
@@ -60,7 +84,7 @@ export const UserConfigurations = () => {
             <div className={'user-configuration-input-label'}>
               <span>Constraseña</span>
               <div className={'user-configuration-input'}>
-                <input type="password" disabled={inputPassword} />
+                <CommonInput type="password" disabled={inputPassword} placeholder= "***************" inputChange={handlePass}/>
                 <img src={editIcon} onClick={() => setInputPassword(false)}/>
               </div>
             </div>
@@ -74,7 +98,7 @@ export const UserConfigurations = () => {
             <WhiteButton title={'Cancelar cambios'}/>
             </div>
             <div>
-            <CommonButton title={'Guardar cambios'}/>
+            <CommonButton title={'Guardar cambios'} onClick={modificar}/>
             </div>
           </div>
         </div>
